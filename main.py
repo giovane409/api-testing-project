711034bf6028e024bd65c91701a89ed6071827cd
Giovane Cardoso <gic@interact.com.br>
2026-09-01 14:13:52 -0300

    feat: implement task CRUD

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 tasks = []
+
+class TaskCreate(BaseModel):
+    title: str
+
+class Task(BaseModel):
+    id: int
+    title: str
+    completed: bool = False
+
+class TaskUpdate(BaseModel):
+    title: str
+    completed: bool
+
 
 @app.get("/")
 def root():
@@ -15,12 +29,43 @@ def get_tasks():
 
 
 @app.post("/tasks")
-def create_task(title: str):
-    task = {
-        "id": len(tasks) +1,
-        "title": title,
-        "completed": False
-    }
+def create_task(task: TaskCreate):
+    new_task = Task (
+        id= len(tasks) +1,
+        title= task.title
+    )
+    tasks.append(new_task)
+    return new_task
 
-    tasks.append(task)
-    return task
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task_update: TaskUpdate):
+    for task in tasks:
+        if task.id == task_id:
+            task.title = task_update.title
+            task.completed = task_update.completed
+            return task
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted"}
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
